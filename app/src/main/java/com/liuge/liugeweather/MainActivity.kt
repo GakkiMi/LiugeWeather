b3263039c9e4759e6d54779c5dbe6d06283337b6
GakkiMi <1902366297@qq.com>
2020-11-24 10:41:45 +0800

    提交内容到geng分支

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("", "----创建了分支");
+        Log.i("", "----我是主分支master");
     }
 }
