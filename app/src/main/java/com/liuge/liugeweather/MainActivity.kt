60d1f4bf8ec26ab1cb51c091abaaf1b1fe76e081
GakkiMi <1902366297@qq.com>
2020-11-24 15:06:26 +0800

    提交master

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -8,9 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("MainActivity", "----窗前明月光")
-        Log.i("MainActivity", "----疑是地上霜")
-        Log.i("MainActivity", "----举头望明月")
-
+        Log.i("MainActivity", "----低头思故乡")
     }
 }
