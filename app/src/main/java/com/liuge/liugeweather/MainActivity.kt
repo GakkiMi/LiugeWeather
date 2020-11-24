6e4b6e031fdabf4001304c4ca8164ce21a000f07
GakkiMi <1902366297@qq.com>
2020-11-24 14:40:48 +0800

    提交master

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -9,9 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("MainActivity", "----你好,我是dev.bro")
-        Toast.makeText(this,"我是在dev分支上开发的",Toast.LENGTH_SHORT).show()
-        Log.i("MainActivity", "----dev.bro新加了一行代码");
+        Log.i("MainActivity", "----疑是地上霜")
+
 
     }
 }
