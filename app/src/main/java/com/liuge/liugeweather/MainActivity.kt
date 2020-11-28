0fb6df4832f8311be5508cc313de38a5441c0ef0
GakkiMi <1902366297@qq.com>
2020-11-28 09:48:43 +0800

    branchA>55555

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         Log.i("MainActivity", "--branchA--22222")
         Log.i("MainActivity", "--branchA--33333")
         Log.i("MainActivity", "--branchA--44444")
+        Log.i("MainActivity", "--branchA--55555")
     }
 }
