31c2f900eb283fac6019e482f17d05900ed30b4a
GakkiMi <1902366297@qq.com>
2020-11-28 09:32:06 +0800

    branchA>11111

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -8,10 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("MainActivity", "--branchA--窗前明月光")
-        Log.i("MainActivity", "--branchB--疑是地上霜")
-        Log.i("MainActivity", "--branchA--举头望明月")
-        Log.i("MainActivity", "--branchB--低头思故乡")
-        Log.i("MainActivity", "--branchA--《静夜思》")
+        Log.i("MainActivity", "--branchA--11111")
     }
 }
