4527ac7e7b2d1b6c4da65655f1061780b78ab582
GakkiMi <1902366297@qq.com>
2020-12-01 16:38:56 +0800

    branchA提交到远程

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -9,11 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("MainActivity", "--branchA--11111")
-        Log.i("MainActivity", "--branchA--22222")
-        Log.i("MainActivity", "--branchA--33333")
-        Log.i("MainActivity", "--branchA--44444")
-        Log.i("MainActivity", "--branchA--55555")
-        Log.i("MainActivity", "--branchA--66666")
+        Log.i("MainActivity", "--branchA--axiba")
+
     }
 }
