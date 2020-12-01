8dabdcc4a88f68947234a52e3ed9373c99ea52c3
GakkiMi <1902366297@qq.com>
2020-12-01 16:47:29 +0800

    ccccccc

--- app/src/main/java/com/liuge/liugeweather/MainActivity.kt
+++ app/src/main/java/com/liuge/liugeweather/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.i("MainActivity", "--branchA--axiba")
+        Log.i("MainActivity", "--branchA--ccccccccc")
 
     }
 }
